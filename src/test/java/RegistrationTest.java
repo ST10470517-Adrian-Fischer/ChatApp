0d4525a2d4a9cfca80ba7add69d1ab83d7631d4d
Adrian Anton Fischer <ST10470517@vcconnect.edu.za>
2025-08-03 13:03:28 +0200

    Update RegistrationTest.java

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
+
 
 import com.mycompany.mavenproject5.Registration;
 import org.junit.jupiter.api.AfterEach;
@@ -11,10 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author nolaf
- */
+
 public class RegistrationTest {
 
     public RegistrationTest() {
