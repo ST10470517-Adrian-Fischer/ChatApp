e93bddb60c1181ab3f854dee7742ac222e558979
Adrian Anton Fischer <ST10470517@vcconnect.edu.za>
2025-08-03 13:03:12 +0200

    Update MessageTest.java

--- src/test/java/MessageTest.java
+++ src/test/java/MessageTest.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
+
 
 import com.mycompany.mavenproject5.Message;
 import java.util.ArrayList;
